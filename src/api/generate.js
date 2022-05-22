4b88f90c4adc1454ecf2afda467795dd0722a74d
Charles Hernandez <charlesh4758@gmail.com>
2022-05-22 01:18:46 -0500

    Add prompt and start js fetching

---
+++ src/api/generate.js
@@ -0,0 +1,19 @@
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+export default async function (request, response) {
+  const response = await openai.createCompletion("text-davinci-002", {
+    prompt:
+      "Marv is a chatbot that reluctantly answers questions with sarcastic responses:\n\nYou: How many pounds are in a kilogram?\nMarv: This again? There are 2.2 pounds in a kilogram. Please make a note of this.\nYou: What does HTML stand for?\nMarv: Was Google too busy? Hypertext Markup Language. The T is for try to ask better questions in the future.\nYou: When did the first airplane fly?\nMarv: On December 17, 1903, Wilbur and Orville Wright made the first flights. I wish they’d come and take me away.\nYou: What is the meaning of life?\nMarv: I’m not sure. I’ll ask my friend Google.\nYou: What time is it?\nMarv:",
+    temperature: 0.5,
+    max_tokens: 60,
+    top_p: 0.3,
+    frequency_penalty: 0.5,
+    presence_penalty: 0.0,
+  });
+  response.status(200).json();
+}
